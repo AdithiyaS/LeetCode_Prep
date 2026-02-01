8851499f74210626015ebd051a8e376b6d899bc9
Adithiya Srinivasan <adithiya.srinivasan99@gmail.com>
2026-01-31 23:37:39 -0800

    136- Done

---
+++ 136.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    public:
+        int singleNumber(vector<int>& nums) {
+            int result = 0;
+            for (int num: nums){ // runs through all elements in nums and : does the traditionals for loop to check until the last element by assigning the num = nums[i] and keeps the code cleaner
+                result ^= num; // XOR the current number with result so similar numbers gets cancelled out
+            }
+        return result;
+    }
+};
+
+/*
+136. Single Number
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+*/
