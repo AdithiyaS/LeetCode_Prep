7845e52e50866a64e9e42ccc7c21406cd32336cb
Adithiya Srinivasan <adithiya.srinivasan99@gmail.com>
2026-02-17 13:08:24 -0800

    1456- Done

---
+++ 1456.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        auto isVowel = [&](char c) {
+            return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+        };
+
+        int currentV = 0;
+        int maxV = 0;
+
+        // first window
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s[i])) currentV++;
+        }
+
+        maxV = currentV;
+        if (maxV == k) return k;
+
+        // slide window
+        for (int i = k; i < (int)s.size(); i++) {
+            if (isVowel(s[i])) currentV++;
+            if (isVowel(s[i - k])) currentV--;
+
+            maxV = max(maxV, currentV);
+            if (maxV == k) return k;
+        }
+
+        return maxV;
+    }
+};
+
+/*
+1456. Maximum Number of Vowels in a Substring of Given Length
+
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+ 
+
+Example 1:
+
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+Example 2:
+
+Input: s = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+Example 3:
+
+Input: s = "leetcode", k = 3
+Output: 2
+Explanation: "lee", "eet" and "ode" contain 2 vowels.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+1 <= k <= s.length
+*/
