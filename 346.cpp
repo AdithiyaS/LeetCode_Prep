1aafe5416a7738ed2990ae195269568749cbafcf
Adithiya Srinivasan <adithiya.srinivasan99@gmail.com>
2026-02-24 10:26:43 -0800

    346- Done

---
+++ 346.cpp
@@ -0,0 +1,61 @@
+class MovingAverage {
+    int cap;
+    queue<int> q;
+    long sum = 0;
+
+public:
+    MovingAverage(int size) : cap(size) {}
+
+    double next(int val) {
+        q.push(val);
+        sum += val;
+
+        if ((int)q.size() > cap) {
+            sum -= q.front();
+            q.pop();
+        }
+
+        int denom = (int)q.size();
+        return (double)sum / denom;
+    }
+};
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage* obj = new MovingAverage(size);
+ * double param_1 = obj->next(val);
+ */
+
+/*
+346. Moving Average from Data Stream
+
+Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+Implement the MovingAverage class:
+
+MovingAverage(int size) Initializes the object with the size of the window size.
+double next(int val) Returns the moving average of the last size values of the stream.
+ 
+
+Example 1:
+
+Input
+["MovingAverage", "next", "next", "next", "next"]
+[[3], [1], [10], [3], [5]]
+Output
+[null, 1.0, 5.5, 4.66667, 6.0]
+
+Explanation
+MovingAverage movingAverage = new MovingAverage(3);
+movingAverage.next(1); // return 1.0 = 1 / 1
+movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+ 
+
+Constraints:
+
+1 <= size <= 1000
+-105 <= val <= 105
+At most 104 calls will be made to next.
+*/
