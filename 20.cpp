0982eed87c12e1976e68f5dbd0628387d3e4dd83
Adithiya Srinivasan <adithiya.srinivasan99@gmail.com>
2026-03-24 21:02:23 -0700

    20- Done

---
+++ 20.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = (int) s.size();
+        if(n % 2) return false;
+        unordered_map<char, char> br = {{'}','{'}, {')','('}, {']','['}};
+        vector<char> valid;
+        for(int i = 0; i< n; i++){
+            if( s[i] == '{' || s[i] == '(' || s[i] == '['){
+                valid.push_back(s[i]);
+                }
+            else{
+                if(valid.empty() || valid.back() != br[s[i]]) return false;
+                valid.pop_back();
+            }
+        }
+        return valid.empty();
+    }
+};
+
+/*
+20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+*/
